22d5044f270eacf67f43ef657286738b6ac36390
Kari Cheslock <karisd5274@gmail.com>
2022-08-15 15:44:44 -0700

    Initial commit

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{ width: 100, height: 100, backgroundColor: 'red' }}></View>
+        <View style={{ width: 100, height: 100, backgroundColor: 'blue' }}></View>
+      </View>
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },

51ef7f83322c79d2676e1e7fb0c2d43aac05acdd
Kari Cheslock <karisd5274@gmail.com>
2022-08-18 16:51:51 -0700

    Next commit

--- App.js
+++ App.js
@@ -62,6 +62,10 @@ export default function App() {
         <Box color={'green'} />
         <Box color={'blue'} />
       </View>
+      <View style={styles.layout}>
+        <View style={styles.card} />
+        <View style={styles.card} />
+      </View>
     </ScrollView>
   </>
   );
@@ -79,4 +83,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  layout: {
+    flex: 1, 
+    backgroundColor: '#e5e5e5', 
+    justifyContent: 'center'
+  },
+  card: {
+    width: 100, 
+    height: 100, 
+    backgroundColor: 'white', 
+    margin: 16, 
+    borderRadius: 2, 
+    shadowColor: 'black', 
+    shadowOpacity: 0.3, 
+    shadowRadius: 1, 
+    shadowOffset: { height: 1, width: 0.3 }
+  }
 });
